cf1db4019a14db4e3fda2c60a930c81987360d01
ult-sohtome <a.sohtome@ultinet.co.jp>
2025-04-07 15:39:48 +0900

    変数名・if文の修正

--- js/migration/runMigrations.js
+++ js/migration/runMigrations.js
@@ -1,24 +1,22 @@
 import { CommentDataMigration_20250403 } from "./commentDataMigration_20250403.js";
 import { CommentDataMigration_20250404 } from "./commentDataMigration_20250404.js";
 
-const MIGRATION_FLAG_TRUE = Object.freeze('true');
-
 const migrations = [
   {
-    version: 'is_comment_data_migrated_20250403',
+    migrationFlagKey: 'is_comment_data_migrated_20250403',
     migrate: (postRepository) => new CommentDataMigration_20250403(postRepository)
   },
   {
-    version: 'is_comment_data_migrated_20250404',
+    migrationFlagKey: 'is_comment_data_migrated_20250404',
     migrate: (postRepository) => new CommentDataMigration_20250404(postRepository)
   }
 ];
 
 export function runMigrations(postRepository) {
-  migrations.forEach(({ version, migrate }) => {
-    if(localStorage.getItem(version) !== MIGRATION_FLAG_TRUE){
+  migrations.forEach(({ migrationFlagKey, migrate }) => {
+    if(localStorage.getItem(migrationFlagKey) !== 'true'){
       migrate(postRepository);
-      localStorage.setItem(version, MIGRATION_FLAG_TRUE);
+      localStorage.setItem(migrationFlagKey, 'true');
     }
   });
 }
