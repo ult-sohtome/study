f8b98cd424a92ba69a5833b925ab2c2f1b6ea59a
ult-sohtome <a.sohtome@ultinet.co.jp>
2025-04-23 11:45:24 +0900

    メソッド名の変更

--- js/parts/styleModeChange.js
+++ js/parts/styleModeChange.js
@@ -4,11 +4,11 @@ export class StyleModeChange {
   constructor(postRepository){
     this.postRepository = postRepository;
     this.modeSelectElem = PostViewRenderer.getStyleModeSelectElem();
-    this.checkStyleMode();
+    this.applyStyleMode();
     this.setupEventListener();
   }
 
-  checkStyleMode(){
+  applyStyleMode(){
     if(this.postRepository.isDarkMode()){
       PostViewRenderer.switchDarkMode();
       this.modeSelectElem.value = this.postRepository.getDarkMode();
@@ -22,7 +22,7 @@ export class StyleModeChange {
     this.modeSelectElem.addEventListener("change", () => {
       const selectedMode = this.modeSelectElem.value;
       this.postRepository.setSelectedMode(selectedMode);
-      this.checkStyleMode();
+      this.applyStyleMode();
     });
   }
 }
