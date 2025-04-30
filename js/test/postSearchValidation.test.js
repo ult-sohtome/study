c11fd67157c4532813f323bbc8f92d6561fbea75
ult-sohtome <a.sohtome@ultinet.co.jp>
2025-04-30 14:53:42 +0900

    テストを纏めるように修正

--- js/test/postSearchValidation.test.js
+++ js/test/postSearchValidation.test.js
@@ -3,51 +3,38 @@ import { PostSearchValidation } from "../validation/postSearchValidation.js";
 const expect = chai.expect;
 
 describe("PostSearchValidation", () => {
-  describe("Keyword_異常系", () => {
-    const testCases = [
-      {
-        description: "検索キーワードが空だと無効",
-        input: "",
-        expected: { isValid: false, errorMessage: "検索キーワードを入力してください。" }
-      },
-      {
-        description: "検索キーワードがnullだと無効",
-        input: null,
-        expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
-      },
-      {
-        description: "検索キーワードが数値型だと無効",
-        input: 123,
-        expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
-      }
-    ];
+  const testCases = [
+    {
+      description: "検索キーワードが空だと無効",
+      input: "",
+      expected: { isValid: false, errorMessage: "検索キーワードを入力してください。" }
+    },
+    {
+      description: "検索キーワードがnullだと無効",
+      input: null,
+      expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
+    },
+    {
+      description: "検索キーワードが数値型だと無効",
+      input: 123,
+      expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
+    },
+    {
+      description: "検索キーワードが1文字は有効",
+      input: "あ",
+      expected: { isValid: true }
+    },
+    {
+      description: "文字の混在は有効",
+      input: "あaい😊23１",
+      expected: { isValid: true }
+    }
+  ];
 
-    testCases.forEach(({ description, input, expected }) => {
-      it(description, () => {
-        const result = PostSearchValidation.validateSearchKeyword(input);
-        expect(result).to.deep.equal(expected);
-      });
-    });
-  });
-  describe("Keyword_正常系", () => {
-    const testCases = [
-      {
-        description: "検索キーワードが1文字は有効",
-        input: "あ",
-        expected: { isValid: true }
-      },
-      {
-        description: "文字の混在は有効",
-        input: "あaい😊23１",
-        expected: { isValid: true }
-      }
-    ];
-
-    testCases.forEach(({ description, input, expected }) => {
-      it(description, () => {
-        const result = PostSearchValidation.validateSearchKeyword(input);
-        expect(result).to.deep.equal(expected);
-      });
+  testCases.forEach(({ description, input, expected }) => {
+    it(description, () => {
+      const result = PostSearchValidation.validateSearchKeyword(input);
+      expect(result).to.deep.equal(expected);
     });
   });
 });
--- js/test/postValidation.test.js
+++ js/test/postValidation.test.js
@@ -3,121 +3,95 @@ import { PostValidation } from "../validation/postValidation.js";
 const expect = chai.expect;
 
 describe("UserNameValidation", () => {
-  describe("UserName_異常系", () => {
-    const testCases = [
-      {
-        description: "数値型を渡すと無効",
-        input: 123,
-        expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
-      },
-      {
-        description: "nullを渡すと無効",
-        input: null,
-        expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
-      },
-      {
-        description: "ニックネームが20文字を超えると無効",
-        input: String("あ").repeat(21),
-        expected: { isValid: false, errorMessage: "20文字以内でニックネームを入力してください。" }
-      },
-    ];
+  const testCases = [
+    {
+      description: "数値型を渡すと無効",
+      input: 123,
+      expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
+    },
+    {
+      description: "nullを渡すと無効",
+      input: null,
+      expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
+    },
+    {
+      description: "ニックネームが20文字を超えると無効",
+      input: String("あ").repeat(21),
+      expected: { isValid: false, errorMessage: "20文字以内でニックネームを入力してください。" }
+    },
+    {
+      description: "ニックネームが空は有効",
+      input: "",
+      expected: { isValid: true }
+    },
+    {
+      description: "ニックネームが1文字は有効",
+      input: "a",
+      expected: { isValid: true }
+    },
+    {
+      description: "文字の混在は有効",
+      input: "あaい😊23１",
+      expected: { isValid: true }
+    },
+    {
+      description: "ニックネームが20文字は有効",
+      input: "😊".repeat(20),
+      expected: { isValid: true }
+    }
+  ];
 
-    testCases.forEach(({ description, input, expected }) => {
-      it(description, () => {
-        const result = PostValidation.validateUserName(input);
-        expect(result).to.deep.equal(expected);
-      });
-    });
-  });
-  describe("UserName_正常系", () => {
-    const testCases = [
-      {
-        description: "ニックネームが空は有効",
-        input: "",
-        expected: { isValid: true }
-      },
-      {
-        description: "ニックネームが1文字は有効",
-        input: "a",
-        expected: { isValid: true }
-      },
-      {
-        description: "文字の混在は有効",
-        input: "あaい😊23１",
-        expected: { isValid: true }
-      },
-      {
-        description: "ニックネームが20文字は有効",
-        input: "😊".repeat(20),
-        expected: { isValid: true }
-      }
-    ];
-
-    testCases.forEach(({ description, input, expected }) => {
-      it(description, () => {
-        const result = PostValidation.validateUserName(input);
-        expect(result).to.deep.equal(expected);
-      });
+  testCases.forEach(({ description, input, expected }) => {
+    it(description, () => {
+      const result = PostValidation.validateUserName(input);
+      expect(result).to.deep.equal(expected);
     });
   });
 });
 
 describe("CommentValidation", () => {
-  describe("Comment_異常系", () => {
-    const testCases = [
-      {
-        description: "数値型を渡すと無効",
-        input: 123,
-        expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
-      },
-      {
-        description: "nullを渡すと無効",
-        input: null,
-        expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
-      },
-      {
-        description: "コメントが空だと無効",
-        input: "",
-        expected: { isValid: false, errorMessage: "コメントを入力してください。" }
-      },
-      {
-        description: "コメントが255文字を超えると無効",
-        input: String("あ").repeat(256),
-        expected: { isValid: false, errorMessage: "255文字以内でコメントを入力してください。" }
-      }
-    ];
+  const testCases = [
+    {
+      description: "数値型を渡すと無効",
+      input: 123,
+      expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
+    },
+    {
+      description: "nullを渡すと無効",
+      input: null,
+      expected: { isValid: false, errorMessage: "不正な値が入力されました。" }
+    },
+    {
+      description: "コメントが空だと無効",
+      input: "",
+      expected: { isValid: false, errorMessage: "コメントを入力してください。" }
+    },
+    {
+      description: "コメントが255文字を超えると無効",
+      input: String("あ").repeat(256),
+      expected: { isValid: false, errorMessage: "255文字以内でコメントを入力してください。" }
+    },
+    {
+      description: "1文字は有効",
+      input: "a",
+      expected: { isValid: true }
+    },
+    {
+      description: "文字の混在は有効",
+      input: "あaい😊23１",
+      expected: { isValid: true }
+    },
+    {
+      description: "255文字は有効",
+      input: String("あ").repeat(255),
+      expected: { isValid: true }
+    }
+  ];
 
-    testCases.forEach(({ description, input, expected }) => {
-      it(description, () => {
-        const result = PostValidation.validateCreateComment(input);
-        expect(result).to.deep.equal(expected);
-      });
-    });
-  });
-  describe("Comment_正常系", () => {
-    const testCases = [
-      {
-        description: "1文字は有効",
-        input: "a",
-        expected: { isValid: true }
-      },
-      {
-        description: "文字の混在は有効",
-        input: "あaい😊23１",
-        expected: { isValid: true }
-      },
-      {
-        description: "255文字は有効",
-        input: String("あ").repeat(255),
-        expected: { isValid: true }
-      }
-    ];
-  
-    testCases.forEach(({ description, input, expected }) => {
-      it(description, () => {
-        const result = PostValidation.validateCreateComment(input);
-        expect(result).to.deep.equal(expected);
-      });
+  testCases.forEach(({ description, input, expected }) => {
+    it(description, () => {
+      const result = PostValidation.validateCreateComment(input);
+      expect(result).to.deep.equal(expected);
     });
   });
 });
